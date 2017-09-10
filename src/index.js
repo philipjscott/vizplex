68f925e23d35f3ad88fe9bfe7bd49cf82ad94a1c
Philip Scott <pscott@zeptohost.com>
2017-09-10 15:48:06 -0400

    Add visualizer component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
-var module = require('./hello-world')
-module()
+// for now use glsl toy
+
+import vert from './vert.glsl'
+import frag from './frag.glsl'
+
+var canvas = document.createElement('canvas')
+document.body.appendChild(canvas)
+
+var clear = require('gl-clear')({ color: [0, 1, 1, 1] })
+var gl = require('gl-context')(canvas, render)
+var glShader = require('gl-shader')
+var fit = require('canvas-fit')
+var now = require('right-now')
+var draw = require('a-big-triangle')
+
+console.log(vert, frag)
+
+var shader = glShader(gl, vert, frag)
+
+var time = now()
+
+function animate () {
+  time = now()
+}
+function render () {
+  var width = gl.drawingBufferWidth
+  var height = gl.drawingBufferHeight
+
+  animate()
+  clear(gl)
+  gl.viewport(0, 0, width, height)
+
+  shader.bind()
+  shader.uniforms.iGlobalTime = time / 1000
+  shader.attributes.position.pointer()
+  draw(gl)
+}
+
+window.addEventListener('resize', fit(canvas), false)
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './docs'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      }
+    ]
   }
 }
